84d28a4bf1fd3dcce4c207fcc08bc618f7cccd7a
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-08 14:18:50 +0200

    2e commit - ajout .gitignore

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,23 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte varByte = -25;
+		short varShort = 2;
+		int varInt = 15;
+		long varLong = 1500L;
+		float varFloat = 2.5f;
+		double varDouble = 0.258;
+		char varChar = '&';
+		boolean varBoolean = true;
+		String varString = "test01";
+
+		System.out.println("varInt : " + varInt + "\n");
+		
+		String randomString = "Voici le résultat d’un calcul :\n1+5=6";
+		System.out.println(randomString);
+		
+	}
+
+}
